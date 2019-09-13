629a1f944feb55ef92590c736f8016d3c0feb7aa
alexanderjones19 <alexanderjones4215@gmail.com>
2019-09-12 23:02:48 -0400

    initial commit and refactor of template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+    title: "Alexander Jones - Software Developer",
+    author: "Alexander Jones",
+    description: "Portfolio website showcasing the work of Alexander Jones"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,73 +3,73 @@ import React, { Component } from 'react';
 import Carousel, { Modal, ModalGateway } from "react-images";
 
 class Gallery extends Component {
-    constructor () {
-        super();
+  constructor () {
+    super();
 
-        this.state = {
-            lightboxIsOpen: false,
-            selectedIndex: 0
-        };
-        
-        this.toggleLightbox = this.toggleLightbox.bind(this);
-    }
-    toggleLightbox(selectedIndex) {
-        this.setState(state => ({
-            lightboxIsOpen: !state.lightboxIsOpen,
-            selectedIndex
-        }));
-    }
-    renderGallery (images) {
-        if (!images) return;
+    this.state = {
+      lightboxIsOpen: false,
+      selectedIndex: 0
+    };
+    
+    this.toggleLightbox = this.toggleLightbox.bind(this);
+  }
+  toggleLightbox(selectedIndex) {
+    this.setState(state => ({
+      lightboxIsOpen: !state.lightboxIsOpen,
+      selectedIndex
+    }));
+  }
+  renderGallery (images) {
+    if (!images) return;
 
-        const gallery = images.map((obj, i) => {
-            return (
-                <article className="6u 12u$(xsmall) work-item" key={i}>
-                    <a
-                        className="image fit thumb"
-                        href={obj.source}
-                        onClick={e => {
-                            e.preventDefault();
-                            this.toggleLightbox(i);
-                        }}
-                    >
-                        <img src={obj.thumbnail} />
-                    </a>
+    const gallery = images.map((obj, i) => {
+      return (
+        <article className="6u 12u$(xsmall) work-item" key={i}>
+          <a
+            className="image fit thumb"
+            href={obj.source}
+            onClick={e => {
+              e.preventDefault();
+              this.toggleLightbox(i);
+            }}
+          >
+            <img src={obj.thumbnail} />
+          </a>
 
-                    <h3>{obj.caption}</h3>
-                    <p>{obj.description}</p>
-                </article>
-            );
-        });
+          <h3>{obj.caption}</h3>
+          <p>{obj.description}</p>
+        </article>
+      );
+    });
 
-        return (
-            <div className="row">
-                {gallery}
-            </div>
-        );
-    }
-    render () {
-        const { images } = this.props;
-        const { selectedIndex, lightboxIsOpen } = this.state;
+    return (
+      <div className="row">
+        {gallery}
+      </div>
+    );
+  }
+  render () {
+    const { images } = this.props;
+    const { selectedIndex, lightboxIsOpen } = this.state;
 
-        return (
-            <div>
-                {this.renderGallery(images)}
-                <ModalGateway>
-                    {lightboxIsOpen && (
-                        <Modal onClose={this.toggleLightbox}>
-                            <Carousel currentIndex={selectedIndex} views={images} />
-                        </Modal>
-                    )}
-                </ModalGateway>
-            </div>
-        );
-    }
-}
+    return (
+      <div>
+        {this.renderGallery(images)}
+        <ModalGateway>
+          {lightboxIsOpen && (
+            <Modal onClose={this.toggleLightbox}>
+              <Carousel currentIndex={selectedIndex} views={images} />
+            </Modal>
+          )}
+        </ModalGateway>
+      </div>
+    );
+  }
+};
 
 Gallery.displayName = 'Gallery';
 Gallery.propTypes = {
-    images: PropTypes.array
+  images: PropTypes.array
 };
 
 export default Gallery;
