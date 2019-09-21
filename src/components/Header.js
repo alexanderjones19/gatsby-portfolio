0c0a785e592a208759a7667f2fdb343e924acd5c
alexanderjones19 <alexanderjones4215@gmail.com>
2019-09-21 16:42:27 -0400

    refactor header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import Footer from './Footer'
 import avatar from '../assets/images/me_portrait.jpg'
 
 const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
+
   return (
     <header id="header">
       <div className="inner">
-        <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-        <h1><strong>I am Strata</strong>, a super simple<br />
-        responsive site template freebie<br />
-        crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+        <Link to="/" className="image avatar"><img src={avatar} alt="Alexander Jones" /></Link>
+        <h1><strong>{data.site.siteMetadata.author}</strong></h1>
+        <h1>Software Developer</h1>
       </div>
       <Footer />
     </header>
